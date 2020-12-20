15381a528fbc48af7b5b7e73fc09bfcacf7829bb
Andrei Dragotoniu <guardinfo-spam@yahoo.com>
2020-12-20 10:32:45 +0000

    add tests project. initial tests where initialized basket calculated value is zero

--- Basket/BasketLib/Basket.cs
+++ Basket/BasketLib/Basket.cs
@@ -10,5 +10,22 @@ namespace BasketLib
         public List<GiftVoucher> GiftVouchers { get; set; }
 
         public OfferVoucher OfferVoucher { get; set; }
+
+        public Basket()
+        {
+            //make sure the lists are initialised ad not null
+            this.ProductItems = new List<ProductItem>();
+            this.GiftVouchers = new List<GiftVoucher>();
+        }
+        
+        public float CalculateValue()
+        {
+            if (this.ProductItems.Count == 0)
+                return 0;
+
+            else
+                return 0;
+        }
+
     }
 }
